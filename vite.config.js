838bdd7a2838fcd25c5bf5e52a732f013d5efee4
rhildred <rhildred@gmail.com>
2023-08-27 16:45:43 +1000

    add params to ssr

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import preact from '@preact/preset-vite'
 import ssr from 'vite-plugin-ssr/plugin'
 
 const config = {
-  plugins: [preact(), ssr()],
+  plugins: [preact(), 
+    ssr({
+      prerender: true,
+      includeAssetsImportedByServer: true,
+    })],
   // We manually add a list of dependencies to be pre-bundled, in order to avoid a page reload at dev start which breaks vite-plugin-ssr's CI
   optimizeDeps: {
     include: ['preact/devtools', 'preact/debug', 'preact/jsx-dev-runtime', 'preact', 'preact/hooks']
